524e0f873e57f81441298462512f550c2d538112
arij_belaid <aarijbelaid@gmail.com>
2025-11-18 09:20:15 +0100

    Ajout de la fonction acces_netmiko

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+# main.py 
+import netmiko from connecthandler 
+
+def acces_netmiko {
+   sandbox-iosxr-1.cisco.com
+   Login: admin
+   Pass: C1sco12345
+   ssh port: 22
+}
+
+def acces_netmiko():
+# Affiche la date coté routeur
+# Affiche les interfaces du routeur dans un fichier interfaces.txt
 print("Hello, Git!")
+dire_bonjour()
+  
+
+    
