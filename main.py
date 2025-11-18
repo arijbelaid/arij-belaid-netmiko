0d222a2d655ab464896395c9d4693c7c763b59d1
arij_belaid <aarijbelaid@gmail.com>
2025-11-18 10:11:56 +0100

    Ajout de la fonction acces_netmiko

--- main.py
+++ main.py
@@ -1,4 +1,27 @@
+from netmiko import ConnectHandler
+
+print("Hello, Git!")
+
+# main.py
+def acces_netmiko():
+# Affiche la date coté routeur
+# Affiche les interfaces du routeur dans un fichier interfaces.txt
+print("Hello, Git!")
+dire_bonjour()
+
+
+def acces_netmiko():
+    device = {
+        "device_type": "cisco_xr",  # pour Cisco C8000V (IOS-XR)
+        "host": "sandbox-iosxr-1.cisco.com",
+        "username": "admin",
+        "password": "C1sco12345",
+        "port": 22,
+    }
+
 def dire_salut():
 print("Salut, Git!")
 dire_salut()
+
+
     
